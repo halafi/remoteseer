cc87f848a9f24e753fc5d8814380c71d214e2933
halafi <filiphalas74@gmail.com>
2019-08-20 15:54:45 +0200

    feat: text

--- src/client/scenes/Root.jsx
+++ src/client/scenes/Root.jsx
@@ -1,6 +1,6 @@
 // @flow
 import React from 'react';
-import { Flex } from '@rebass/grid';
+import { Box, Flex } from '@rebass/grid';
 import styled from 'styled-components';
 import { hot } from 'react-hot-loader';
 
@@ -10,9 +10,25 @@ const Container: any = styled(Flex)`
   background-color: ${({ theme }) => theme.primary};
 `;
 
+const Description = styled(Box)`
+  text-align: center;
+  width: 790px;
+`;
+
+const Subheader = styled.h2`
+  font-size: 19px;
+  font-weight: 400;
+`;
+
 const Root = () => (
   <Container alignItems="center" flexDirection="column">
     <h1>Remote Seer</h1>
+    <Description>
+      <Subheader>
+        Find remote work and work from anywhere. We aggregate all the providers we can to bring you
+        the <strong>largest listing of remote jobs</strong>. All remote jobs in one place.
+      </Subheader>
+    </Description>
     <img alt="work remotely" src="images/work_remotely.svg" />
   </Container>
 );
