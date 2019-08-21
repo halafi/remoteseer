35e0fde660b76198a9e7943728cb8448d2e156ce
halafi <filiphalas74@gmail.com>
2019-08-21 20:10:53 +0200

    refactor: datafetch

--- src/server/fetchData.js
+++ src/server/fetchData.js
@@ -1,4 +1,6 @@
 // @flow
+import 'core-js';
+import 'regenerator-runtime/runtime';
 import fetch from 'node-fetch';
 import path from 'path';
 import fs from 'fs-extra';
@@ -6,17 +8,17 @@ import fs from 'fs-extra';
 // eslint-disable-next-line import/prefer-default-export
 export const DATA_DIR = path.resolve(__dirname, '../../data');
 
-fs.ensureDir(DATA_DIR).then(() => {
-  fetch(
-    `https://github-jobs-proxy.appspot.com/positions?utf8=%E2%9C%93&description=&location=remote`,
-  )
-    .then(res => {
-      return res.json();
-    })
-    .then(json => {
-      if (json.type && json.type === 'error') {
-        throw new Error(json.message);
-      }
-      fs.outputJson(path.join(DATA_DIR, 'jobs.json'), json);
-    });
-});
+async function fetchData() {
+  await fs.ensureDir(DATA_DIR);
+  const response = await fetch(
+    'https://github-jobs-proxy.appspot.com/positions?utf8=%E2%9C%93&description=&location=remote',
+  );
+  const data = await response.json();
+
+  if (data.type && data.type === 'error') {
+    throw new Error(data.message);
+  }
+  await fs.outputJson(path.join(DATA_DIR, 'jobs.json'), data);
+}
+
+fetchData();
