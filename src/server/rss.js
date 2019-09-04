3f537da7f99eeac819720e62bf395792036e6c04
halafi <filiphalas74@gmail.com>
2019-09-04 15:28:58 +0200

    feat: update rss

--- src/server/rss.js
+++ src/server/rss.js
@@ -10,8 +10,6 @@ import { getJobs } from './services/dataFiles/index';
 const PATH = path.join(__dirname, '../static/remote-jobs.rss');
 const DOMAIN = 'https://remoteseer.net';
 
-const lastmod = new Date().toISOString();
-
 async function storeXml(filepath: string, xml: string[]) {
   return new Promise((resolve, reject) => {
     const filestream = fs.createWriteStream(filepath, { flags: 'w+', encoding: 'utf8' });
@@ -32,7 +30,7 @@ async function storeXml(filepath: string, xml: string[]) {
   });
 }
 
-async function generateSitemap() {
+async function generateRss() {
   const xml = [
     '<?xml version="1.0" encoding="UTF-8"?>',
     '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">',
@@ -46,16 +44,15 @@ async function generateSitemap() {
 
   const jobs = getJobs('').slice(0, 100);
   jobs.forEach(job => {
-    // 2019-08-08T14:49:24-07:00
     xml.push(
       `<item><guid>${job.url}</guid><title>${job.title.replace(
         '&',
         'and',
-      )}</title><description>${job.company
+      )} at ${job.company
         .replace(/\s+/g, ' ')
-        .replace('&', 'and')}</description><link>${job.url}</link><pubDate>${new Date(
-        job.createdAt,
-      ).toUTCString()}</pubDate></item>`,
+        .replace('&', 'and')}</title><description>Tags: ${job.tags.join(', ')}</description><link>${
+        job.url
+      }</link><pubDate>${new Date(job.createdAt).toUTCString()}</pubDate></item>`,
     );
   });
 
@@ -64,4 +61,4 @@ async function generateSitemap() {
   await storeXml(PATH, xml);
 }
 
-generateSitemap();
+generateRss();
