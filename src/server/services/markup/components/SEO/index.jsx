fea4dde559ecfd0f7cb3633b8a4039051770e523
halafi <filiphalas74@gmail.com>
2019-09-21 17:28:11 +0200

    feat: tmp og image

--- src/server/services/markup/components/SEO/index.jsx
+++ src/server/services/markup/components/SEO/index.jsx
@@ -14,9 +14,12 @@ const SEO = ({ title, description, url }: Props) => (
     <meta property="og:type" content="website" />
     <meta property="og:title" content={title} />
     <meta property="og:url" content={`https://remoteseer.net${url !== '/' ? url : ''}`} />
-    {/* <meta property="og:image" content={socialImage.url} />
-    <meta property="og:image:width" content={socialImage.width} />
-    <meta property="og:image:height" content={socialImage.height} /> */}
+    <meta
+      property="og:image"
+      content="https://remoteseer.net/images/icons/android-chrome-512x512.png"
+    />
+    <meta property="og:image:width" content="512" />
+    <meta property="og:image:height" content="512" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@remote_seer" />
     <meta name="twitter:creator" content="@remote_seer" />
